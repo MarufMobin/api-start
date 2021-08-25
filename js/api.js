09f613697fb26c7ebdf339675b1a378735ee7d4e
unknown <mobinh264@gmail.com>
2021-08-26 02:53:28 +0600

    Index page all kind of Working done

--- js/api.js
+++ js/api.js
@@ -4,21 +4,21 @@
 
 
 /*----------------Using api for Demo Data---------------------*/ 
-// document.getElementById('output').addEventListener('click',function(){
-//      fetch('https://jsonplaceholder.typicode.com/todos/1')
-//      .then( response => response.json())
-//      .then(json => console.log(json))
+document.getElementById('output').addEventListener('click',function(){
+     fetch('https://jsonplaceholder.typicode.com/todos/1')
+     .then( response => response.json())
+     .then(demoData => demoDataCollection(demoData))
 
-// });
+});
 
-
+/* 
 function loadData(){
     
     fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then(responce => responce.json())
     .then( data => console.log(data) )
    
-}
+} */
 /* ---------------Using APi for Users------------------------*/
 /* 
 function users(){
@@ -28,9 +28,7 @@ function users(){
 } */
 
 document.getElementById('usersData').addEventListener('click', function(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then( response => response.json())
-    .then( data => displayUsers(data))
+    fetch('https://jsonplaceholder.typicode.com/users').then( res => res.json()).then( data => displayUsers(data))
 });
 
 /*------------ Using Api for Posts ----------*/
@@ -40,17 +38,60 @@ document.getElementById('usersData').addEventListener('click', function(){
 // }
 
 document.getElementById('posts').addEventListener('click',function(){
-    fetch('https://jsonplaceholder.typicode.com/posts').then( res => res.json()).then( data => console.log(data))
+    fetch('https://jsonplaceholder.typicode.com/posts').then( res => res.json()).then( posts => displayCommnet(posts))
+
 });
 
+// Using api update Ui 
+// function displayUsers(data){
+//     const ul =  document.getElementById('users');
+//     for( const user of data){
+//       const li = document.createElement('li');
+//         li.innerText = `Name : ${user.name} and email : ${user.email}`;
 
-function displayUsers(data){
-    const ul =  document.getElementById('users');
-    for( const user of data){
-      const li = document.createElement('li');
-        li.innerText = `Name : ${user.name} ALso email : ${user.email}`;
-
-      ul.appendChild(li);
+//       ul.appendChild(li);
        
+//     }
+// }
+
+// Once More time make a function displayUsers function to
+// -------------------------------------------------------
+function displayUsers(data){
+    const usersBtn = document.getElementById('users');
+    for( const singleData of data){
+        const mkAli = document.createElement('li');
+        mkAli.innerText = singleData.name;
+        usersBtn.appendChild(mkAli)
     }
+}
+
+
+
+function displayCommnet(posts){
+    const ulForCom = document.getElementById('users');
+    for( const singlePost of posts){
+        const mkLi = document.createElement('li');
+        mkLi.innerText = singlePost.title;
+        ulForCom.appendChild(mkLi)
+    }
+}
+
+function reload(){
+    // location.reload()
+    window.location.href='index.html'
+}
+
+
+// Usign api to fetch data for demo 
+
+function demoDataCollection(demoData){
+    const demoInput = document.getElementById('users');
+    const mkLiOfDemo = document.createElement('li');
+    mkLiOfDemo.innerText = demoData.title
+    demoInput.appendChild(mkLiOfDemo)  
+
+}
+
+function newPage(){
+    location.href = "posts.html";
 }
